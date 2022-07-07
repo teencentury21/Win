e408499c929e11b900b9899d555ef7ef194674d4
LAI PEI-JHOU <teencentury21@gmail.com>
2022-07-07 14:32:00 +0800

    my first commit

---
+++ SQL_Queston1.sql
@@ -0,0 +1,17 @@
+USE InterView
+Create table Person (personId int, firstName varchar(255), lastName
+varchar(255))
+Create table Address (addressId int, personId int, city
+varchar(255), state varchar(255))
+Truncate table Person
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen')
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob')
+Truncate table Address
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York
+City', 'New York')
+insert into Address (addressId, personId, city, state) values ('2', '3',
+'Leetcode', 'California')
+
+SELECT personId, firstName, lastName  FROM Person 
+SELECT addressId, personId, city, state FROM Address
+SELECT P.firstName, P.lastName, A.city, A.state FROM Person AS P LEFT JOIN Address AS A ON P.personId=A.personId
---
+++ SQL_Queston2.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE InterView
+USE InterView
+Create table Customers (id int, name varchar(255))
+Create table Orders (id int, customerId int)
+Truncate table Customers
+insert into Customers (id, name) values ('1', 'Joe')
+insert into Customers (id, name) values ('2', 'Henry')
+insert into Customers (id, name) values ('3', 'Sam')
+insert into Customers (id, name) values ('4', 'Max')
+Truncate table Orders
+insert into Orders (id, customerId) values ('1', '3')
+insert into Orders (id, customerId) values ('2', '1')
+
+SELECT * FROM Customers
+SELECT * FROM Orders
+SELECT name FROM Orders AS O FULL JOIN Customers AS C ON C.id=O.customerId
+WHERE O.customerId IS NULL
